8799abe8eba4903ff772c4e4e31729de1a015f9e
meowji <meowmobile@gmail.com>
2016-07-27 12:34:49 +0800

    Change URLs

--- sh-4kiptv.sh
+++ sh-4kiptv.sh
@@ -26,7 +26,7 @@ echo 正在删除旧文件
 rm -rf 4k-start*
 #下载开机运行脚本
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/4k-start
+wget -q --no-check-certificate https://raw.githubusercontent.com/meowji/CTSH-4KIPTV/HG680-J_R7000_HG220GS/4k-start
 #设置权限
 chmod -R 0755 4k-start
 #完成提示
@@ -46,7 +46,7 @@ echo 正在删除旧文件
 rm -rf dnsmasq.postconf
 #下载新配置文件
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
+wget -q --no-check-certificate https://raw.githubusercontent.com/meowji/CTSH-4KIPTV/HG680-J_R7000_HG220GS/ss
 #重命名新配置文件
 mv ss dnsmasq.postconf
 #设置权限
@@ -68,7 +68,7 @@ echo 正在删除旧文件
 rm -rf dnsmasq.postconf
 #下载新配置文件
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/noss
+wget -q --no-check-certificate https://raw.githubusercontent.com/meowji/CTSH-4KIPTV/HG680-J_R7000_HG220GS/noss
 #重命名新配置文件
 mv noss dnsmasq.postconf
 #设置权限
